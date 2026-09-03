8ba4a67da1886a928c5d6ddd4fadd551cca33dfa
Anuj Kumar <test@example.com>
2026-09-03 11:56:39 +0530

    pattern

---
+++ 15_pattern.js
@@ -0,0 +1,30 @@
+// patter
+let n=5;
+pattern=(n)=>{
+    for(let i=1;i<=n;i++){
+        let str="";
+        for(let j=1;j<=i;j++){
+            str+="*";
+        }
+        console.log(str);
+    
+    }   
+}
+pattern(n);
+
+
+
+
+
+// pattern=()=>{
+
+    
+//     for(let i=1;i<=n;i++){
+//         for(let j=1;j<=i;j++){
+//             console.log("*");
+//         }
+//         console.log("");
+
+//     }
+// }
+// pattern();
