f236124faa9942a8f7e14bfb2da2cb614ed6ad6a
Anuj Kumar <test@example.com>
2026-09-22 10:51:15 +0530

    .\19_question.js

--- 19_question.js
+++ 19_question.js
@@ -28,9 +28,52 @@ s3.displayResult();
 console.log("Total Students:", student.count);
 
 console.log("--------------------------------------------------\n");
+// /2. Bank Account Management System
+// Create a class BankAccount with accountNo, holderName, and balance. Use a constructor to initialize the account. Provide instance methods deposit(amount), withdraw(amount), and displayBalance(). Withdrawal should not be allowed when the requested amount is greater than the available balance. Create a static method bankInfo() that displays the bank name and general banking information. Create two account objects and perform different transactions on them.
+// Concepts Covered: Constructor, Objects, Instance Methods, Static Method, Object State
 
+class BankAccount{
 
+ constructor(accountNo, holderName,balance){
+this.accountNo=accountNo;
+this.holderName=holderName;
+this.balance=balance;
+}   
 
+deposit(amount){
+    this.balance += amount;
+    console.log("Deposited:", amount, "New Balance:", this.balance);
+}
+withdraw(amount){
+    if(amount> this.balance){
+        console.log("Insufficient Balance");
+    } else{
+        this.balance -= amount;
+        console.log("Withdrawn:", amount, "New Balance:", this.balance);
+    }
+}
+static bankinfo(){
+    console.log("Bank Name: XYZ Bank");
+    console.log("Branch: Main Branch");
+    console.log("IFSC Code: XYZ123456");
+}
+displayInfo(){
+       BankAccount.bankinfo();
+    console.log("Account No:", this.accountNo);
+    console.log("Holder Name:", this.holderName);
+    console.log("Balance:", this.balance);
+ 
+}
 
+};
+let b1=new BankAccount(12345,"dhruv",1000);
+let b2=new BankAccount(12346,"anuj",2000);
+b1.deposit(500);
+b1.withdraw(200);
+b1.displayInfo();
+b2.deposit(1000);
+b2.withdraw(3000);
+b2.displayInfo();
 
+console.log("--------------------------------------------------\n");
 
